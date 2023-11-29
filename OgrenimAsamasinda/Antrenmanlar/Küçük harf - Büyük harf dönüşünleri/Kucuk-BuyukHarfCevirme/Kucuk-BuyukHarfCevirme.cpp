4644f8419d89fd257c471be8c36de0d107815ec7
ibrahim-alikilic <ibrahim.alikilic@gmail.com>
2023-11-29 08:27:32 +0300

    Küçük büyük harf dönüşümü

---
+++ OgrenimAsamasinda/Antrenmanlar/Küçük harf - Büyük harf dönüşünleri/Kucuk-BuyukHarfCevirme/Kucuk-BuyukHarfCevirme.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+void Trans(char* harf) {
+    for (int i = 0; harf[i] != '\0'; i++) {
+        harf[i] = tolower(harf[i]);
+    }
+}
+int main()
+{
+    setlocale(LC_ALL, "Turkish");
+    char Metin[] = "bİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ Bilgisayar Mühendisliği Bölümü";
+    Trans(Metin);
+    cout << Metin << endl;
+}
+
